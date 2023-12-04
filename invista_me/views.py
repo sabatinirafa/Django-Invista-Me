b0a0cfe38b50796cbedcfdbacc64807e7ba12212
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-12-04 00:08:44 -0300

    Inicialização do projeto

---
+++ invista_me/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def pagina_inicial(request):
+    return HttpResponse('<h1>Pronto para investir</h1>')
+
+def contato(request):
+    return HttpResponse('<h1>Página de contato</h1>')
+
+def minha_historia(request):
+    return render(request,'investimentos/minha_historia.html')
