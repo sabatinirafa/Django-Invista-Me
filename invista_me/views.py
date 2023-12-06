5f35105b0ef302f04bfb65286b2aaf4b7bb758b8
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-12-06 14:02:01 -0300

    Alteração e exclusão de dados

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from django.shortcuts import HttpResponse
 from .models import Investimento
+from .forms import InvestimentoForm
 
-def novo_investimento(request):
-    return render(request, 'investimentos/novo_investimento.html')
 
 
 def investimentos(request):
@@ -17,3 +16,34 @@ def detalhe(request, id_investimento):
         'dados': Investimento.objects.get(pk=id_investimento)
     }
     return render(request, 'investimentos/detalhe.html', dados)
+
+def criar(request):
+    if request.method == 'POST':
+        investimento_form = InvestimentoForm(request.POST)
+        if investimento_form.is_valid():
+            investimento_form.save()
+        return redirect('investimentos')
+    else:
+        investimento_form = InvestimentoForm()
+        formulario = {
+            'formulario': investimento_form
+        }
+        return render(request, 'investimentos/novo_investimento.html', formulario)
+    
+def editar(request, id_investimento):
+    investimento = Investimento.objects.get(pk=id_investimento)
+    if request.method == 'GET':
+        formulario = InvestimentoForm(instance=investimento)
+        return render(request, 'investimentos/novo_investimento.html', {'formulario': formulario})
+    else:
+        formulario = InvestimentoForm(request.POST, instance=investimento)
+        if formulario.is_valid():
+            formulario.save()
+        return redirect('investimentos')
+    
+def excluir(request, id_investimento):
+    investimento = Investimento.objects.get(pk=id_investimento)
+    if request.method == 'POST':
+        investimento.delete()
+        return redirect('investimentos')
+    return render(request, 'investimentos/confirmar_exclusao.html', {'item': investimento})
