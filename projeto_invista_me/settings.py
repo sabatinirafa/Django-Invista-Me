69b9505d4b6971c3219d3dcd6f01bbd4f5bc01c0
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-12-07 16:49:11 -0300

    Conclusão

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'jWZHrZJk2UdTf2MP',
+        'HOST': 'db.hhniskspidrhtmoquuiv.supabase.co',
+        'PORT': '5432'
     }
 }
 
