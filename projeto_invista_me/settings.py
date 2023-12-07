dac8741e7faccddc95951fd7b074907dde9d6573
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-12-06 21:07:27 -0300

    Criação de cadastro de usuários

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invista_me'
+    'invista_me',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
